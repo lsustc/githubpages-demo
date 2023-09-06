beceec20bfc12abbf0be021da0f00f2b4c9aad69
liushuai <lsustc@mail.ustc.edu.cn>
2023-09-06 10:38:29 +0800

    feat update

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js", "githubpages-demo/dist/precache-manifest.db10694ed9a684c3588a1701a6a12492.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js", "precache-manifest.cb407d3f4237803c41145dc5dec2fa3b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef*/
 if (workbox) {
